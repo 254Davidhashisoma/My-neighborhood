d787a934dad27e0ea4ec57a5146215ea470e5d47
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-14 09:41:45 +0300

    added URL DIR

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,19 @@
 
-from django.urls import path, include
+ffrom django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    path('',views.index,name = 'index'),
-    path('profile/<str:username>/',views.profile,name = 'profile'),
-    path('hoods/',views.hoods,name = 'hoods'),
-    path('single_hood/<hood_id>',views.single_hood,name = 'single_hood'),
-    path('results/',views.search_profile,name = 'results'),
-    path('join_hood/<id>',views.join,name='join'),
-    path('leave_hood/<id>',views.leave,name='leave'),
-    path('api/profile/', views.ProfileList.as_view()),
-    path('api/users/', views.UserList.as_view()),
+    url('^$',views.home,name = 'home'),
+    url(r'^profile/', views.profile, name='profile'),
+    url('^neighbourhood', views.neighbourhood, name='neighbourhood'),
+    url('^addneighbourhood',views.addneighbourhood,name="addneighbourhood"),
+    url(r'^detail/(?P<neighbourhood_id>\d+)/$' , views.neighbourhood_details, name='detail' ),
+    url(r'^new_business/(?P<pk>\d+)$',views.new_business,name='new_business'),
+    url(r'^new_post/(?P<pk>\d+)$',views.new_post,name='new_post'),
+    url(r'^search/',views.search_hoods,name='search_hoods'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
